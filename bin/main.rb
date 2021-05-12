7483297e36ef1f26ace0b2f1ac29356bb6d940d6
tmampa <tmampa94@gmail.com>
2021-05-12 02:26:12 +0200

    Fix linter errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,8 +1,6 @@
-# rubocop:disable Style/FrozenStringLiteralComment
 # rubocop:disable Layout/LeadingCommentSpace
 
 #!/usr/bin/env ruby
 puts 'hello, world'
 
 # rubocop:enable Style/FrozenStringLiteralComment
-# rubocop:enable Layout/LeadingCommentSpace
