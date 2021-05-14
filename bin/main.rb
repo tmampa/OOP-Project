5ce226e9933bb95c3a8f958b5c985010257ea82a
Mwila Kaunda <mbkcust@gmail.com>
2021-05-14 11:53:54 +0200

    Initialize board

--- bin/main.rb
+++ bin/main.rb
@@ -24,8 +24,6 @@ def turn_sequence(player, symbol)
   if (1..9).include?(@player_move) && @gameboard.board[@player_move] == ' ' && @end == false
     @turn += 1
     @gameboard.board_update(@player_move, symbol)
-    win_check
-    draw_check
   else
     puts "Please enter a number between 1 to 9 in an emmpty location\n"
   end
