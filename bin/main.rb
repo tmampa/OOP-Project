a3d15637de8d3a62b1bac224927cd2e70c8e7f53
tmampa <tmampa94@gmail.com>
2021-05-14 11:37:20 +0200

    Add method to display the board

--- bin/main.rb
+++ bin/main.rb
@@ -56,6 +56,14 @@ class Board
     puts '6 | 7 | 8'
     @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
   end
+
+  def game_board_display(board)
+    puts "#{board[0]} | #{board[1]} | #{board[2]}"
+    puts '---------'
+    puts "#{board[3]} | #{board[4]} | #{board[5]}"
+    puts '---------'
+    puts "#{board[6]} | #{board[7]} | #{board[8]}"
+  end
 end
 
 players = Players.new
