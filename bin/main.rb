2accd7056f84c51e8bc461c587cb751cb8dfc223
tmampa <tmampa94@gmail.com>
2021-05-14 11:21:04 +0200

    Add a method for turn sequence

--- bin/main.rb
+++ bin/main.rb
@@ -18,6 +18,19 @@ class Game
   end
 end
 
+def turn_sequence(player, symbol)
+  puts "#{player}(#{symbol}) please choose a position"
+  @player_move = gets.chomp.to_i
+  if (1..9).include?(@player_move) && @gameboard.board[@player_move] == ' ' && @end == false
+    @turn += 1
+    @gameboard.board_update(@player_move, symbol)
+    win_check
+    draw_check
+  else
+    puts "Please enter a number between 1 to 9 in an emmpty location\n"
+  end
+end
+
 class Players
 end
 
