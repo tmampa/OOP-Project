a69a53a34f5ce9d44ec237e446a2e2282fb4b5e5
Mwila Kaunda <mbkcust@gmail.com>
2021-05-14 12:38:10 +0200

    Fix board error

--- bin/main.rb
+++ bin/main.rb
@@ -10,20 +10,20 @@ class Game
     @turn = 1
     while @turn < 10
       if @turn.odd?
-        turn_sequence(@players.player1, 'X')
+        turn_sequence(@players.player1,)
       elsif @turn.even?
-        turn_sequence(@players.player2, 'O')
+        turn_sequence(@players.player2,)
       end
     end
   end
 end
 
-def turn_sequence(player, symbol)
-  puts "#{player}(#{symbol}) please choose a position"
+def turn_sequence(player)
+  puts "#{player} please choose a position"
   @player_move = gets.chomp.to_i
   if (1..9).include?(@player_move) && @gameboard.board[@player_move] == ' ' && @end == false
     @turn += 1
-    @gameboard.board_update(@player_move, symbol)
+    @gameboard.board_update
   else
     puts "Please enter a number between 1 to 9 in an emmpty location\n"
   end
@@ -47,16 +47,17 @@ class Board
 
   def initialize
     puts 'On your turn enter one of the following numbers to place your piece in the corresponding location:'
-    puts '0 | 1 | 2'
-    puts '---------'
-    puts '3 | 4 | 5'
-    puts '---------'
-    puts '6 | 7 | 8'
+    # puts '0 | 1 | 2'
+    # puts '---------'
+    # puts '3 | 4 | 5'
+    # puts '---------'
+    # puts '6 | 7 | 8'
     @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    board_update
   end
 
-  def board_update(position, symbol)
-    @board[position] = symbol
+  def board_update
+    # @board[position] = symbol
     game_board_display(@board)
   end
 
