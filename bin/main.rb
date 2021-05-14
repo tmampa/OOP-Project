cff4893f2998ba8fff16f1da30db8c8f7bdb9e10
Mwila Kaunda <mbkcust@gmail.com>
2021-05-14 10:49:31 +0200

    Add move method to game class

--- bin/main.rb
+++ bin/main.rb
@@ -3,6 +3,19 @@ class Game
   def initialize
     @players = Players.new
   end
+
+  def move
+    @end = false
+    @gameboard = Board.new
+    @turn = 1
+    while @turn < 10
+      if @turn.odd?
+        turn_sequence(@players.player1, 'X')
+      elsif @turn.even?
+        turn_sequence(@players.player2, 'O')
+      end
+    end
+  end
 end
 
 class Players
