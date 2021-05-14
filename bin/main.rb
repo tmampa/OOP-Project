d7ef267ba2d4747475e6e964435a087dbcda8493
Mwila Kaunda <mbkcust@gmail.com>
2021-05-14 11:27:01 +0200

    Initialize Player class

--- bin/main.rb
+++ bin/main.rb
@@ -32,6 +32,16 @@ def turn_sequence(player, symbol)
 end
 
 class Players
+  attr_reader :player1, :player2
+
+  def initialize
+    puts 'Player 1, please enter your name'
+    @player1 = gets.chomp
+    puts "#{@player1} is X"
+    puts 'Player 2, please enter your name'
+    @player2 = gets.chomp
+    puts "#{@player2} is O"
+  end
 end
 
 class Board
